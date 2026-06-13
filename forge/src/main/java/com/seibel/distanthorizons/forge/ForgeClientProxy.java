7ec06d4a5e692baaa414fea58e960f2ed3d663d0
Fabian Maurer <dark.shadow4@web.de>
2026-06-13 02:21:09 +0200

    Fix compile errors 1.20.1

--- forge/src/main/java/com/seibel/distanthorizons/forge/ForgeClientProxy.java
+++ forge/src/main/java/com/seibel/distanthorizons/forge/ForgeClientProxy.java
@@ -133,7 +133,7 @@ public class ForgeClientProxy implements AbstractModInitializer.IEventProxy
 				executor.execute(() ->
 				{
 					ChunkAccess chunk = level.getChunk(event.getPos());
-					SharedApi.INSTANCE.applyChunkUpdate(new ChunkWrapper(chunk, wrappedLevel), wrappedLevel);
+					SharedApi.INSTANCE.applyChunkUpdate(new ChunkWrapper(chunk, wrappedLevel), wrappedLevel, true);
 				});
 			}
 		}
@@ -161,7 +161,7 @@ public class ForgeClientProxy implements AbstractModInitializer.IEventProxy
 				executor.execute(() ->
 				{
 					ChunkAccess chunk = level.getChunk(event.getPos());
-					SharedApi.INSTANCE.applyChunkUpdate(new ChunkWrapper(chunk, wrappedLevel), wrappedLevel);
+					SharedApi.INSTANCE.applyChunkUpdate(new ChunkWrapper(chunk, wrappedLevel), wrappedLevel, true);
 				});
 			}
 		}
@@ -174,7 +174,7 @@ public class ForgeClientProxy implements AbstractModInitializer.IEventProxy
 		{
 			ILevelWrapper wrappedLevel = ProxyUtil.getLevelWrapper(GetEventLevel(event));
 			IChunkWrapper chunkWrapper = new ChunkWrapper(event.getChunk(), wrappedLevel);
-			SharedApi.INSTANCE.applyChunkUpdate(chunkWrapper, wrappedLevel);
+			SharedApi.INSTANCE.applyChunkUpdate(chunkWrapper, wrappedLevel, true);
 		}
 	}
 	
