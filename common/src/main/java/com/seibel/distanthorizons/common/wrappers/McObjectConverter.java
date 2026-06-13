18500aef085e04895f5d00f193c94b0523dc3d2a
Fabian Maurer <dark.shadow4@web.de>
2026-06-13 09:24:42 +0200

    fix matrix crash

--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/McObjectConverter.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/McObjectConverter.java
@@ -83,7 +83,26 @@ public class McObjectConverter
 			matrix, 
 			FloatBuffer buffer)
 	{
-		#if MC_VER <= MC_1_12_2
+		#if MC_VER == MC_1_7_10
+		// JOML's Matrix4f.get(FloatBuffer) routes through MemUtilUnsafe in the newer JOML
+		// bundled with lwjgl3ify and segfaults on heap buffers, so put each entry manually.
+		buffer.put(bufferIndex(0, 0), matrix.m00());
+		buffer.put(bufferIndex(0, 1), matrix.m01());
+		buffer.put(bufferIndex(0, 2), matrix.m02());
+		buffer.put(bufferIndex(0, 3), matrix.m03());
+		buffer.put(bufferIndex(1, 0), matrix.m10());
+		buffer.put(bufferIndex(1, 1), matrix.m11());
+		buffer.put(bufferIndex(1, 2), matrix.m12());
+		buffer.put(bufferIndex(1, 3), matrix.m13());
+		buffer.put(bufferIndex(2, 0), matrix.m20());
+		buffer.put(bufferIndex(2, 1), matrix.m21());
+		buffer.put(bufferIndex(2, 2), matrix.m22());
+		buffer.put(bufferIndex(2, 3), matrix.m23());
+		buffer.put(bufferIndex(3, 0), matrix.m30());
+		buffer.put(bufferIndex(3, 1), matrix.m31());
+		buffer.put(bufferIndex(3, 2), matrix.m32());
+		buffer.put(bufferIndex(3, 3), matrix.m33());
+		#elif MC_VER <= MC_1_12_2
 		matrix.get(buffer);
         #elif MC_VER < MC_1_19_4
 		matrix.store(buffer);
