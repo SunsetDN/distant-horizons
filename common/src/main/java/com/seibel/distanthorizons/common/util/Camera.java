2cf998d908feabf79f1e7cc8ad8fbfcd58c6dc8b
James Seibel <jeseibel@gondtc.com>
2026-07-25 08:33:57 -0500

    render wrapper 1.7 camera

--- common/src/main/java/com/seibel/distanthorizons/common/util/Camera.java
+++ common/src/main/java/com/seibel/distanthorizons/common/util/Camera.java
@@ -13,41 +13,68 @@ import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
 import com.seibel.distanthorizons.RenderHelper;
 #endif
 
-public class Camera 
+/**
+ * Recreation of MC's camera object
+ * for use with MC 1.7.10.
+ */
+public class Camera
 {
 	#if MC_VER <= MC_1_7_10
 	
-    final Vector3d pos = new Vector3d();
-    final BlockPos blockPos = new BlockPos();
-    float pitch;
-    float yaw;
-    EntityLivingBase entity;
-    boolean thirdPerson;
-    final float partialTicks;
+	public static final Camera INSTANCE = new Camera();
+	
+    private final Vector3d pos = new Vector3d();
+    //final BlockPos blockPos = new BlockPos();
+    //float pitch;
+    //float yaw;
+    //EntityLivingBase entity;
+    //boolean thirdPerson;
+    //final float partialTicks;
 	
 	
 	
-    public Vector3d getPos() { return pos; }
-
-    public Camera(EntityLivingBase entity, float partialTicks) 
-    {
-        this.partialTicks = partialTicks;
-        final Vector4f offset = new Vector4f(); // third person offset
-        final Matrix4f inverseModelView = RenderHelper.getModelViewMatrixMC()
-            .invert();
-        inverseModelView.transform(offset);
-
-        final double camX = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * partialTicks + offset.x;
-        final double camY = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * partialTicks + offset.y;
-        final double camZ = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * partialTicks + offset.z;
-        this.entity = entity;
-
-        pos.set(camX, camY, camZ);
-        blockPos.set(MathHelper.floor_double(camX), MathHelper.floor_double(camY), MathHelper.floor_double(camZ));
-        pitch = entity.cameraPitch;
-        yaw = entity.rotationYaw;
-        thirdPerson = Minecraft.getMinecraft().gameSettings.thirdPersonView == 1;
-
-    }
+	//========//
+	// update //
+	//========//
+	//region
+	
+	public void update(EntityLivingBase entity, float partialTicks)
+	{
+		final Vector4f offset = new Vector4f(); // third person offset
+		final Matrix4f inverseModelView = RenderHelper.getModelViewMatrixMC()
+			.invert();
+		inverseModelView.transform(offset);
+		
+		final double camX = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * partialTicks + offset.x;
+		final double camY = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * partialTicks + offset.y;
+		final double camZ = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * partialTicks + offset.z;
+		this.pos.set(camX, camY, camZ);
+		
+		//this.partialTicks = partialTicks;
+		//this.entity = entity;
+		//this.blockPos.set(
+		//	MathHelper.floor_double(camX),
+		//	MathHelper.floor_double(camY),
+		//	MathHelper.floor_double(camZ));
+		//this.pitch = entity.cameraPitch;
+		//this.yaw = entity.rotationYaw;
+		//this.thirdPerson = (Minecraft.getMinecraft().gameSettings.thirdPersonView == 1);
+	}
+	
+	//endregion
+	
+	
+	
+	//=========//
+	// getters //
+	//=========//
+	//region
+	
+	public Vector3d getPos() { return this.pos; }
+	
+	//endregion
+	
+	
+	
 	#endif
 }
--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftRenderWrapper.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftRenderWrapper.java
@@ -256,29 +256,31 @@ public class MinecraftRenderWrapper implements IMinecraftRenderWrapper
 			}
 		}
 		
-		#if MC_VER <= MC_1_12_2
-		#if MC_VER <= MC_1_7_10
-		float frameTime = ((IMixinMinecraft) Minecraft.getMinecraft()).getTimer().renderPartialTicks;
-		Camera camera = new Camera(MC.renderViewEntity, frameTime);
-		Vector3d projectedView = camera.getPos();
-		return new DhVec3d(projectedView.x, projectedView.y, projectedView.z);
-		#else
-		RenderManager rm = MC.getRenderManager();
-		return new DhVec3d(rm.viewerPosX, rm.viewerPosY, rm.viewerPosZ);
-		#endif
-		#else
-		#if MC_VER <= MC_26_1_2
-		Camera camera = MC.gameRenderer.getMainCamera();
-		#else
-		Camera camera = MC.gameRenderer.mainCamera();
-		#endif
 		
-		#if MC_VER <= MC_1_21_10
-		Vec3 projectedView = camera.getPosition();
+		
+		#if MC_VER <= MC_1_7_10
+		{
+			float frameTime = ((IMixinMinecraft) Minecraft.getMinecraft()).getTimer().renderPartialTicks;
+			Camera.INSTANCE.update(MC.renderViewEntity, frameTime);
+			Vector3d projectedView = Camera.INSTANCE.getPos();
+			return new DhVec3d(projectedView.x, projectedView.y, projectedView.z);
+		}
 		#else
-		Vec3 projectedView = camera.position();
-		#endif
-		return new DhVec3d(projectedView.x, projectedView.y, projectedView.z);
+		{
+			#if MC_VER <= MC_26_1_2
+			Camera camera = MC.gameRenderer.getMainCamera();
+			#else
+			Camera camera = MC.gameRenderer.mainCamera();
+			#endif
+		
+			#if MC_VER <= MC_1_21_10
+			Vec3 projectedView = camera.getPosition();
+			#else
+			Vec3 projectedView = camera.position();
+			#endif
+		
+			return new DhVec3d(projectedView.x, projectedView.y, projectedView.z);
+		}
 		#endif
 	}
 	
